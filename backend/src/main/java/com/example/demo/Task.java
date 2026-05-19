6f56199d19d898a85b6c9802e96c617ce76bcfef
Stefi99 <stephanie.martins.lima@hotmail.com>
2026-05-19 20:22:45 +0200

    feat: implementiere Todo Prioritäten Fügt Prioritäten im Frontend und Backend hinzu.

--- backend/src/main/java/com/example/demo/Task.java
+++ backend/src/main/java/com/example/demo/Task.java
@@ -6,17 +6,34 @@ package com.example.demo;
  */
 public class Task {
 	
-	private String taskdescription; // must have the EXACT name as his React state property and may not be ignored!
+	private String taskdescription; // wichtig: Name muss exakt gleich wie im Frontend sein
+	private String oldTaskdescription;
+	private String priority = "Mittel";
 
 	public Task() {
     }
 
-	public String getTaskdescription() { // do not apply camel-case here! Its a Bean!
+	public String getTaskdescription() {
 		return taskdescription;
 	}
 
-	public void setTaskdescription(String taskdescription) { // do not apply camel-case here! Its a Bean!
+	public void setTaskdescription(String taskdescription) {
 		this.taskdescription = taskdescription;
 	}
 
+	public String getOldTaskdescription() {
+		return oldTaskdescription;
+	}
+
+	public void setOldTaskdescription(String oldTaskdescription) {
+		this.oldTaskdescription = oldTaskdescription;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
 }
